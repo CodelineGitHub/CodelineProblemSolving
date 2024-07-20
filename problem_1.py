079abc911a7ad92881920f4e07c36b885fa49314
AyaAlmqbali <66S1572@shct.edu.om>
2024-07-21 02:54:29 +0400

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -9,5 +9,24 @@ if not username or len(username)>50:
 else:
   print("Username is valid")
 
+if len(password)<8 or not (r'[!@#$%^&*(),.|<>]',password) or not (r'[A-Z]',password) or not (r'[a-z]',password) or not (r'[0-9]',password):
+         print("Password is invalid")
+    else:
+        print("Password is valid")
 
-if len(password)<8 or not (r'[!@
+
+ if (r'^[\w\.-]+@[\w\.-]+\.\w+$', email):
+          print("Password is invalid")
+
+    else:
+
+        print("Password is valid")
+
+
+
+    if (r'^[\w\.-]+@[\w\.-]+\.\w+$', email):
+        print("Email is valid")
+    else:
+        print("Email is invalid")
+
+login()
