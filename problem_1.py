314958a6b9977518503208d4e90109569b1bb3fe
AyaAlmqbali <66S1572@shct.edu.om>
2024-07-21 02:11:47 +0400

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -1,1 +1,13 @@
+In [11]:
+def login():
+  username=input("Username:")
+  password=input("Password:")
+  email=input("Email:")
 
+if not username or len(username)>50:
+  print("Username is invalid")
+else:
+  print("Username is valid")
+
+
+if len(password)<8 or not (r'[
